4b94837a32dd91f661ab5ee219cd85c67a2ac246
Rauno Moisto <rauno.moisto@credy.com>
2018-12-11 11:42:22 +0200

    reverted keycode 229

--- src/MaskedInput.js
+++ src/MaskedInput.js
@@ -129,7 +129,6 @@ export default {
       switch (e.keyCode) {
         // backspace
         case 8:
-        case 229:
           e.preventDefault();
           if (
             this.maskCore.selection.start > this.marginLeft ||
