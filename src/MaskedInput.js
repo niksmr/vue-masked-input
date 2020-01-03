29f9a9ecdd6b7c96bdd551ae0d7318a50cc29bf0
Deniz Kovancı <deniz.kovanci@ogr.deu.edu.tr>
2020-01-03 15:31:35 +0300

    Update MaskedInput.js

--- src/MaskedInput.js
+++ src/MaskedInput.js
@@ -60,7 +60,10 @@ export default {
       }
     },
     value(newValue) {
-      if (this.maskCore) this.maskCore.setValue(newValue); // For multiple inputs support
+      if (this.maskCore) {
+				this.maskCore.setValue(newValue); // For multiple inputs support}
+				this.updateToCoreState();
+			}
     },
   },
 
